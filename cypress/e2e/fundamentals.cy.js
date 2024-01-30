8892c7774f1ad50cba86e550d5659379f7dbcdf2
Tim Moss <tmoss726@hotmail.com>
2024-01-30 14:24:41 -0500

    Test with new Cypress commands

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,15 @@
 describe('Fundamentals Test', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/fundamentals')
+  beforeEach(() => {
+    cy.visit('fundamentals')
+  })
+  it('Contains correct header text', () => {
+    cy.get('[data-test="fundamentals-header"]').should('contain.text', 'Testing Fundamentals')
+  })
+  it('Accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
